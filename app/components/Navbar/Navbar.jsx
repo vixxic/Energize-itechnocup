9ab56e9ef6ab682de6130e463013cdbbf13e0fde
vixxic <janikanilobak@gmail.com>
2026-08-08 12:04:28 +0700

    navbar and hero

---
+++ app/components/Navbar/Navbar.jsx
@@ -0,0 +1,59 @@
+"use client";
+
+import "./Navbar.css";
+import { useEffect, useState } from "react";
+
+import Link from "next/link";
+
+// icons
+import { GiHamburgerMenu } from "react-icons/gi";
+
+function Navbar() {
+  const [scrolled, setScrolled] = useState(false);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      setScrolled(window.scrollY > 50);
+    };
+
+    window.addEventListener("scroll", handleScroll);
+
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, []);
+
+  return (
+    <div className={`navbar-outer  ${scrolled ? "scrolled" : ""}`}>
+      <div className="navbar-inner">
+        <div className="logo-container">
+          <img src="/logo.png" alt="Logo" className="nav-logo" />
+          <p className="navbar-logo">Energize</p>
+        </div>
+
+        <ul className="nav-links">
+          <li className={`${scrolled ? "scrolled" : ""}`}>Beranda</li>
+          <li className={`${scrolled ? "scrolled" : ""}`}>Fitur</li>
+          <li className={`${scrolled ? "scrolled" : ""}`}>Cara Kerja</li>
+          <li className={`${scrolled ? "scrolled" : ""}`}>Dampak</li>
+        </ul>
+
+        <div className="nav-hp">
+          <div className={`analyze-btn ${scrolled ? "scrolled" : ""}`}>
+            <Link href="/dashboard">
+              <button className={`${scrolled ? "scrolled" : ""}`}>
+                Analisis
+              </button>
+            </Link>
+          </div>
+
+          <button className="dropdown-btn-hp">
+            <GiHamburgerMenu />
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Navbar;
---
+++ app/landingPage/Hero.jsx
@@ -0,0 +1,93 @@
+"use client";
+
+import "./Hero.css";
+import { useEffect } from "react";
+
+import gsap from "gsap";
+import { ScrollTrigger } from "gsap/all";
+
+gsap.registerPlugin(ScrollTrigger);
+
+function Hero() {
+  useEffect(() => {
+    const triggers = [];
+
+    gsap.to(".layer1", {
+      y: -50,
+      ease: "none",
+      scrollTrigger: {
+        trigger: ".home-section",
+        start: "10% top",
+        end: "bottom top",
+        scrub: true,
+      },
+    });
+    gsap.to(".layer2", {
+      y: 120,
+      ease: "none",
+      scrollTrigger: {
+        trigger: ".home-section",
+        start: "10% top",
+        end: "bottom top",
+        scrub: true,
+      },
+    });
+    gsap.to(".hero-text-con", {
+      y: 120,
+      ease: "none",
+      scrollTrigger: {
+        trigger: ".home-section",
+        start: "10% top",
+        end: "bottom top",
+        scrub: true,
+      },
+    });
+
+    gsap.to(".layer3", {
+      y: 250,
+      ease: "none",
+      scrollTrigger: {
+        trigger: ".home-section",
+        start: "10% top",
+        end: "bottom top",
+        scrub: true,
+      },
+    });
+
+    return () => {
+      ScrollTrigger.getAll().forEach((trigger) => trigger.kill());
+    };
+  }, []);
+
+  return (
+    <div className="home-section">
+      <div className="absolute-div">
+        <div className="hero-text-con">
+          <p>Pahami Energi Anda</p>
+          <p>
+            Kenali pola penggunaan listrik, temukan peluang penghematan, dan
+            bangun kebiasaan energi yang lebih bijak.
+          </p>
+          <button className="analyze-btn-hero">Analisis Sekarang</button>
+        </div>
+        <img
+          className="layer1"
+          src="/parallax-effect/layer-1-parallax.png"
+          alt="gunung"
+        />
+        <img
+          className="layer2"
+          src="/parallax-effect/layer-2-parallax.png"
+          alt="rumah"
+        />
+        <img
+          className="layer3"
+          src="/parallax-effect/layer-3-parallax.png"
+          alt="bukit"
+        />
+      </div>
+    </div>
+  );
+}
+
+export default Hero;
